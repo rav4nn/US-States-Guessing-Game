1439b2c8ed1b1c7865c511548eb0dbb26b2a8a08
Hardeep Singh <hardeepsindia@gmail.com>
2026-01-31 19:05:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,22 +20,34 @@ fifty_states = pandas.read_csv("50_states.csv")
 
 # number of states guessed correctly
 states_guessed = 0
+guessed_states = []
 
 # main quiz loop
 while states_guessed < 50:
+    #input is saved in answer_state
     answer_state = screen.textinput(title=f"States Guessed = {states_guessed}", prompt="Enter State Name:")
+
+    if answer_state == "exit":
+        print('Exiting Game')
+        missing_states = [state for state in fifty_states.state if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states, columns=["States"])
+        new_data.to_csv("states_to_learn.csv")
+        break
+
     for state_name in fifty_states.state:
         if answer_state.lower() == state_name.lower():
-            # print(fifty_states[fifty_states.state==state_name].x.iloc[0])
-            x = fifty_states[fifty_states.state==state_name].x.iloc[0]
-            y = fifty_states[fifty_states.state==state_name].y.iloc[0]
+            if state_name not in guessed_states:
+                # print(fifty_states[fifty_states.state==state_name].x.iloc[0])
+                x = fifty_states[fifty_states.state==state_name].x.iloc[0]
+                y = fifty_states[fifty_states.state==state_name].y.iloc[0]
 
-            # goto the marked location on the screen and write state name
-            write_name.goto(x,y)
-            write_name.write(state_name, font=FONT)
+                # goto the marked location on the screen and write state name
+                write_name.goto(x,y)
+                write_name.write(state_name, font=FONT)
 
-            # increase the guess count and add it to the screen text
-            states_guessed += 1
+                # increase the guess count and add it to the screen text
+                states_guessed += 1
+                guessed_states.append(state_name)
 
 
 
